3d6e26af1eeed57fb4fcf6541f8a1702968d4617
Chris Shepherd <chris@loneless.org>
2017-07-03 22:38:23 +0200

    Cool mouse tracking

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,18 @@ var menu_icon = document.getElementById("menu_icon");
 var nav_back = document.getElementById("nav_back");
 var nav = document.getElementById("nav");
 
-menu_icon.addEventListener("click", function () {
+function click () {
     nav_back.classList.toggle("open");
     menu_icon.classList.toggle("close");
     nav.classList.toggle("show");
-}, false);
+}
+
+menu_icon.addEventListener("click", click, false);
+
+window.addEventListener("mousemove", function (e) {
+    if (e.pageX <= 0 && !nav_back.classList.contains("open")) {
+        click();
+    } else if (e.pageX > 200 && nav_back.classList.contains("open")) {
+        click();
+    }
+});
